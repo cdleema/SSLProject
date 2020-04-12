d0e467d4acc84e3e90d06320360dace726cc0716
Christopher Dleema <christopherdleema@hotmail.com>
2020-04-12 19:48:25 -0400

    First commit

---
+++ SSLProtocol/src/coe817project/SSLException.java
@@ -0,0 +1,10 @@
+package coe817project;
+
+/**
+ * @author Christopher D'leema
+ */
+public class SSLException extends Exception{
+    public SSLException(String errMsg){
+        super(errMsg);
+    }
+}
